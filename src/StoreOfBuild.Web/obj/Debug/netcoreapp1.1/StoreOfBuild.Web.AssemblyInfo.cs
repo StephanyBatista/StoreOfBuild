a7c776f476774dac1a3e849ca23cd3fe549366de
Stephany Henrique <salmeidabatista@gmail.com>
2017-08-20 12:51:33 -0400

    alterando appsettings

--- src/StoreOfBuild.Web/obj/Debug/netcoreapp1.1/StoreOfBuild.Web.AssemblyInfo.cs
+++ src/StoreOfBuild.Web/obj/Debug/netcoreapp1.1/StoreOfBuild.Web.AssemblyInfo.cs
@@ -7,11 +7,11 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("storeofbuild.web")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("StoreOfBuild.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Package Description")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("storeofbuild.web")]
-[assembly: System.Reflection.AssemblyTitleAttribute("storeofbuild.web")]
+[assembly: System.Reflection.AssemblyProductAttribute("StoreOfBuild.Web")]
+[assembly: System.Reflection.AssemblyTitleAttribute("StoreOfBuild.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
